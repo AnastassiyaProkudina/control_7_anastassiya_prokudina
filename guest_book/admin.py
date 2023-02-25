f78e7515e2f4a0b0ad3c1b81086a2f7d968ac44e
Anastassiya Prokudina <anakess88@gmail.com>
2023-02-25 19:24:14 +0600

    Добавила admin.py
    Внесла регистрацию модели в админ панель

---
+++ guest_book/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from guest_book.models import GuestBook
+
+
+class GuestBookAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "email",
+        "text",
+        "status",
+        "created_at",
+        "updated_at",
+        "is_deleted",
+        "deleted_at",
+    )
+    list_filter = ("name",)
+    fields = ("name", "email", "text")
+
+
+admin.site.register(GuestBook, GuestBookAdmin)
